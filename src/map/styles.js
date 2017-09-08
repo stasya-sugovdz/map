a9f2e34349fea2cb4750062836c02c167fe84d01
stasya_sugovdz <ssugovdz@qualium-systems.com.ua>
2017-09-08 11:23:00 +0300

    added MapContainer component

---
+++ src/map/styles.js
@@ -0,0 +1,90 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get("window");
+
+const CARD_HEIGHT = height / 4;
+const CARD_WIDTH = CARD_HEIGHT - 50;
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    sidebar: {
+        width: '20%',
+        backgroundColor: '#ddd'
+
+    },
+    mapContainer: {
+        flex: 1
+    },
+    circle: {
+        height: 20,
+        width: 20,
+        borderRadius: 20 / 2,
+        backgroundColor: '#fff',
+        borderWidth: 5,
+        borderColor: '#800C0C'
+    },
+    scrollView: {
+        position: "absolute",
+        bottom: 30,
+        left: 0,
+        right: 0,
+        paddingVertical: 10,
+    },
+    endPadding: {
+        paddingRight: width - CARD_WIDTH,
+    },
+    card: {
+        padding: 10,
+        elevation: 2,
+        backgroundColor: "#FFF",
+        marginHorizontal: 10,
+        shadowColor: "#000",
+        shadowRadius: 5,
+        shadowOpacity: 0.3,
+        shadowOffset: { x: 2, y: -2 },
+        height: CARD_HEIGHT,
+        width: CARD_WIDTH,
+        overflow: "hidden",
+    },
+    cardImage: {
+        flex: 3,
+        width: "100%",
+        height: "100%",
+        alignSelf: "center",
+    },
+    textContent: {
+        flex: 1,
+    },
+    cardtitle: {
+        fontSize: 12,
+        marginTop: 5,
+        fontWeight: "bold",
+    },
+    cardDescription: {
+        fontSize: 12,
+        color: "#444",
+    },
+    markerWrap: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    marker: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        backgroundColor: "rgba(130,4,150, 0.9)",
+    },
+    ring: {
+        width: 24,
+        height: 24,
+        borderRadius: 12,
+        backgroundColor: "rgba(130,4,150, 0.3)",
+        position: "absolute",
+        borderWidth: 1,
+        borderColor: "rgba(130,4,150, 0.5)",
+    },
+});
+
